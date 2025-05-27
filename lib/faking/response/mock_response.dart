cf45b0100a084afbf84bce6a18fb4e1171f85050
Vinícius Flores <viniciusfribeiro1207@gmail.com>
2025-05-27 09:08:56 -0300

    adds fixture source

--- lib/faking/response/mock_response.dart
+++ lib/faking/response/mock_response.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
-
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:saloon/faking/response/fixture_type.dart';
 import 'package:saloon/saloon.dart';
 
 typedef MockResponseWhen = bool Function(PendingRequest pending);
@@ -19,9 +20,14 @@ class MockResponse {
   static Future<MockResponse> fromFixture(
     String path, {
     required Type request,
+    FixtureSource source = FixtureSource.file,
     MockResponseWhen? when,
   }) async {
-    final fixture = await File(path).readAsString();
+    final fixture = switch (source) {
+      FixtureSource.asset => await rootBundle.loadString(path),
+      FixtureSource.file => await File(path).readAsString(),
+    };
+
     final json = jsonDecode(fixture);
 
     final response = json['body'] is Map
